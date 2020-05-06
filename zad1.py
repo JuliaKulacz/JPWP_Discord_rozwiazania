744e4e8130c9ab6e99bc56073e77c12f3de1bb93
JuliaKulacz <44200333+JuliaKulacz@users.noreply.github.com>
2020-05-06 10:08:36 +0200

    Zadanie 1

---
+++ zad1.py
@@ -0,0 +1,5 @@
+import requests
+
+req = requests.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+with open('C:/Users/jurus/Documents/AGH/JPWP/jakis_plik.csv', 'w') as csv:
+    csv.write(req.text)
